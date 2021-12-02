56faedb31d36042b8e3e0ac74b99cf22c65ea049
Jonathan Nguyen <jnganguyen3@gmail.com>
2021-12-01 22:36:59 -0500

    Commit before a pull

--- finalproject.py
+++ finalproject.py
@@ -1,12 +1,10 @@
-# AUTHOR: Jonathan Nguyen & Austin Porter
 import numpy as np
 
+
 def main():
-    heist_data = np.genfromtxt('adult.data', delimiter=',', skip_header=1)
-    print(heist_data)
-    print("Hello world!")
+    census_data = np.genfromtxt('adult.data', delimiter=',', skip_header=1)
+    print(census_data)
 
 
 if __name__ == "__main__":
     main()
-
